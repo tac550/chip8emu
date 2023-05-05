2db5f3deb0b7a95538fcd107a898690d668aa394
Thomas Crosby <tac550@gmail.com>
2023-05-04 21:31:26 -0400

    Test add function stores result by writing to passed struct reference

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@
 pub struct Chip8State {
     left: u8,
     right: u8,
+    total: u8,
 }
 
 #[no_mangle]
 pub extern "C" fn chip8_add(state: &mut Chip8State) -> i32 {
-    (state.left + state.right).into()
+    state.total = state.left + state.right;
+    0
 }
 
 #[cfg(test)]
@@ -15,8 +17,8 @@ mod tests {
 
     #[test]
     fn test_chip8_add() {
-        let mut state = Chip8State {left: 2, right: 2};
-        let result = chip8_add(&mut state);
-        assert_eq!(result, 4);
+        let mut state = Chip8State {left: 2, right: 2, total: 0};
+        chip8_add(&mut state);
+        assert_eq!(state.total, 4);
     }
 }
