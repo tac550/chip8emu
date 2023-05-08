d175229394433b05cccf1816d47ad82bc2b78405
Thomas Crosby <tac550@gmail.com>
2023-05-08 01:22:58 -0400

    Implement write_fb

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,47 @@ impl Chip8State {
             self.st -= 1;
         }
     }
+
+    /// XORs one byte to the framebuffer at specified location.
+    /// Returns true if any active bit was overwritten.
+    /// 
+    /// # Arguments
+    ///
+    /// * `x` and `y` coordinates - The leftmost destination pixel of the byte being drawn
+    fn write_fb(&mut self, pixels: u8, x: u8, y: u8) -> bool {
+        let x_offset = (x % 8) as usize; // First operation because we shadow the original value of x after this.
+        let x = x / 8; // Eliminate non-byte-aligned values for calculating relevant byte indices in fb
+
+        let x = x % 8;
+        let y = y % 32;
+
+        let first_index = ((8 * y) + x) as usize;
+
+        if x < 7 {
+            let second_index = first_index + 1;
+
+            let left_pixels = pixels >> x_offset;
+            let right_pixels = shl_or(pixels, 8 - x_offset, 0);
+            let left_oldval = self.framebuffer[first_index];
+            let right_oldval = self.framebuffer[second_index];
+
+            self.framebuffer[first_index] = left_oldval ^ left_pixels;
+            self.framebuffer[second_index] = right_oldval ^ right_pixels;
+
+            left_oldval & left_pixels != 0 || right_oldval & right_pixels != 0
+        } else {
+            let pixels = pixels >> x_offset;
+            let oldval = self.framebuffer[first_index];
+            self.framebuffer[first_index] = oldval ^ pixels;
+    
+            oldval & pixels != 0
+        }
+    }
+}
+
+/// Shift left without risk of overflow errors
+const fn shl_or(val: u8, shift: usize, def: u8) -> u8 {
+    [val << (shift & 7), def][((shift & !7) != 0) as usize]
 }
 
 #[no_mangle]
@@ -81,5 +122,38 @@ mod tests {
         }
 
         assert_eq!((state.dt, state.st), (0, 0))
+    }
+
+    #[test]
+    fn test_shl_or() {
+        assert_eq!(shl_or(5, 6, 0), 64);
+        assert_eq!(shl_or(5, 8, 0), 0)
+    }
+    
+    #[test]
+    fn test_write_fb_byte_aligned() {
+        let mut state = Chip8State::default();
+
+        assert!(!state.write_fb(0xFF, 0, 0));
+        assert!(!state.write_fb(0xAB, 8, 1));
+        assert!(!state.write_fb(0x12, 56, 31));
+        assert!(state.write_fb(0x2, 56, 31));
+
+        assert_eq!(state.framebuffer[0], 0xFF);
+        assert_eq!(state.framebuffer[9], 0xAB);
+        assert_eq!(state.framebuffer[255], 0x10)
+    }
+    #[test]
+    fn test_write_fb_not_byte_aligned() {
+        let mut state = Chip8State::default();
+
+        assert!(!state.write_fb(0xFF, 1, 0));
+        assert!(state.write_fb(0xEF, 5, 0));
+        assert!(!state.write_fb(0xFF, 59, 10));
+
+        assert_eq!(state.framebuffer[0], 0x78);
+        assert_eq!(state.framebuffer[1], 0xF8);
+        assert_eq!(state.framebuffer[87], 0x1F);
+        assert_eq!(state.framebuffer[88], 0x00)
     }
 }
