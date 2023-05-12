d86544dfa452edf054922ef316b1ffaa12549473
Thomas Crosby <tac550@gmail.com>
2023-05-11 22:58:09 -0400

    Make Reg enum Clone and Copy

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod sprite;
 const INSTR_SIZE: u8 = 2;
 
 /// For conveniently accessing registers in a 16-byte buffer
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Reg {
     V0 = 0, V1 = 1, V2 = 2, V3 = 3, V4 = 4, V5 = 5, V6 = 6, V7 = 7,
     V8 = 8, V9 = 9, VA = 10, VB = 11, VC = 12, VD = 13, VE = 14, VF = 15,
